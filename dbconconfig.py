59cf10c75c60edf85e303887c4b2af57a5d66a4b
Riley <rileyruckman@gmail.com>
2022-10-05 19:14:22 -0700

    Removed config file creation tool

--- dbconconfig.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config["DEFAULT"] = {
-    "host" : "localhost",
-    "database" : "ruckmans"
-}
-with open("dbcon.cfg", "w") as configfile:
-    config.write(configfile)
